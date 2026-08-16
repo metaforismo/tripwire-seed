2a1992bb37808a0f453c95bd863563d9341cb849
metaforismo <39646696+metaforismo@users.noreply.github.com>
2026-08-16 23:48:58 +0200

    Restore the official BIP39 seed vector

--- src/wallet.rs
+++ src/wallet.rs
@@ -203,7 +203,7 @@ mod tests {
         let seed = mnemonic.to_seed("TREZOR");
         assert_eq!(
             seed.as_hex().to_string(),
-            "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a698759d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04"
+            "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04"
         );
     }
 
